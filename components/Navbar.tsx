f6dd2d4ef7a41f31a32dae6c88f220f93d63429e
Mohammadreza <moharnadreza@gmail.com>
2021-05-24 19:05:26 +0430

    fix: fixed logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar: FC = (): JSX.Element => {
   return (
     <div className="bg-gray-800 py-6 md:py-8 px-8 md:px-12 my-10 rounded-lg flex items-center">
       <div className="flex-1 flex items-center">
-        <Image src="/logo.svg" width={24} height={24} />
+        <Image src="/logo.png" width={36} height={36} />
         <span className="text-xs ml-4 md:text-sm md:flex">jsonPlaceholder</span>
       </div>
       <Link
