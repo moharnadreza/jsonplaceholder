4a3ecc71d02cbe020e0aa049e90727a9c5af762d
Mohammadreza <moharnadreza@gmail.com>
2021-05-24 19:07:11 +0430

    feat: updated page title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { useCallback, useState, useMemo } from "react";
+import Head from "next/head";
 
 import { Link } from "components";
 import { SampleCode, SampleCodePlayground } from "sections";
@@ -28,38 +29,46 @@ const Home = (): JSX.Element => {
   }, []);
 
   return (
-    <div className="flex flex-col text-center py-12 md:py-32 max-w-xl m-auto">
-      <h1>jsonPlaceholder</h1>
-      <p className="leading-loose max-w-md m-auto mt-8">
-        RTL fake REST/GraphQL API for testing and prototyping, based on{" "}
-        <Link
-          label={`{JSON}Placeholder`}
-          href="https://github.com/typicode/jsonplaceholder"
-          title="A simple online fake REST API server by @typicode."
-        />
-        .
-      </p>
-      <div className="w-full flex flex-wrap flex-col md:flex-row items-center justify-center mt-8 space-y-6 md:space-y-0 md:space-x-6">
-        <Link
-          variant="button"
-          label="GraphQL Playground"
-          href="http://jsonplaceholder.ir/graphql"
-          className="bg-graphql-light hover:bg-graphql-dark"
-        />
-        <Link
-          variant="button"
-          label="Donation"
-          href="http://zarinp.al/@moharnadreza"
-        />
+    <>
+      <Head>
+        <title>
+          jsonPlaceholder | RTL fake REST/GraphQL API for testing and
+          prototyping.
+        </title>
+      </Head>
+      <div className="flex flex-col text-center py-12 md:py-32 max-w-xl m-auto">
+        <h1>jsonPlaceholder</h1>
+        <p className="leading-loose max-w-md m-auto mt-8">
+          RTL fake REST/GraphQL API for testing and prototyping, based on{" "}
+          <Link
+            label={`{JSON}Placeholder`}
+            href="https://github.com/typicode/jsonplaceholder"
+            title="A simple online fake REST API server by @typicode."
+          />
+          .
+        </p>
+        <div className="w-full flex flex-wrap flex-col md:flex-row items-center justify-center mt-8 space-y-6 md:space-y-0 md:space-x-6">
+          <Link
+            variant="button"
+            label="GraphQL Playground"
+            href="http://jsonplaceholder.ir/graphql"
+            className="bg-graphql-light hover:bg-graphql-dark"
+          />
+          <Link
+            variant="button"
+            label="Donation"
+            href="http://zarinp.al/@moharnadreza"
+          />
+        </div>
+        <div className="flex flex-col mt-12 md:mt-32 w-full">
+          <SampleCodePlayground
+            onMethodChange={handleMethodChange}
+            onResolverChange={handleResolverChange}
+          />
+          <SampleCode sampleCode={sampleCode} onCopy={handleCopy} />
+        </div>
       </div>
-      <div className="flex flex-col mt-12 md:mt-32 w-full">
-        <SampleCodePlayground
-          onMethodChange={handleMethodChange}
-          onResolverChange={handleResolverChange}
-        />
-        <SampleCode sampleCode={sampleCode} onCopy={handleCopy} />
-      </div>
-    </div>
+    </>
   );
 };
 
